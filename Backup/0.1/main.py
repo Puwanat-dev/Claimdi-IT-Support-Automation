d5be739d0048b2be2dedc972884871ae607b890d
cQMan <manhodmak@gmail.com>
2026-08-26 11:37:37 +0700

    Remove untracked files and update .gitignore

--- Backup/0.1/main.py
+++
@@ -1,8 +0,0 @@
-from App_UI import App_UI
-from ImagePacker import ImagePacker
-
-if __name__ == "__main__":
-    app2 = ImagePacker(input("Input the Picture path: "))
-    app2.process()
-    #app = App_UI()
-    #app.mainloop()
--- Backup/0.1/test.py
+++
@@ -1,2 +0,0 @@
-x = "https://pic.claimdi.com/bike/ImageInspection/2024/Sep/14/CLM-INS-2409191338/PT04/2c8da5ad-386c-4cec-9a7d-aef543d9545d.Jpeg"
-print(x[45:75])
